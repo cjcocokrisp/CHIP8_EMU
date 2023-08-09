704a07a5ccb2074663da1ed2e6acac83f770f787
Christopher Coco <chriscoco1205@gmail.com>
2023-08-09 19:35:15 -0400

    init repo

---
+++ main.c
@@ -0,0 +1,21 @@
+// TODO: include graphics stuff
+// TODO: import chip8 object
+
+int main(int argc, char* argv[])
+{
+    // TODO: Set up render system and register input callbacks
+
+    // TODO: Initialize the chip8 system and load the program into memory
+
+    // Emulation Loop
+    for(;;)
+    {
+        // TODO: emulate one cycle
+
+        // TODO: If draw flag is set, update the screen
+
+        // TODO: Store key press state (Press and release)
+    }
+
+    return 0;
+}
